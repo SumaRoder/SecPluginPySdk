6fa1710a71f9d42b70c18cf7fc036b6ea25fa38a
sumaroder <mcbbsxiaoliuaabb@outlook.com>
2025-07-17 21:48:25 +0800

    20250717-1.2

---
+++ plugin/Msg.py
@@ -0,0 +1,232 @@
+from enum import Enum
+
+class Msg(Enum):
+    """消息类型集合。
+
+    包含所有支持的消息类型常量。
+    """
+    # 基础消息类型
+    AtAll = "AtAll"  # 艾特全体成员
+    AtUin = "AtUin"  # 被艾特账号
+    AtName = "AtName"  # 被艾特昵称
+    Id = "Id"  # ID
+    Ok = "Ok"  # 状态码
+    No = "No"  # 状态码
+    Op = "Op"  # 操作
+    OpUid = "OpUid"  # 操作
+    All = "All"  # 所有
+    Get = "Get"  # 得到
+    Gif = "Gif"  # 动图
+    Img = "Img"  # 图片
+    Ptt = "Ptt"  # 语音
+    Uid = "Uid"  # 账号
+    Uin = "Uin"  # 账号
+    Url = "Url"  # 链接
+    Xml = "Xml"  # XML
+    Code = "Code"  # 码
+    Info = "Info"  # 信息
+    Json = "Json"  # JSON
+    Text = "Text"  # 文本消息
+    Temp = "Temp"  # 临时消息
+    Time = "Time"  # 时间
+    Type = "Type"  # 类型
+    Emoid = "Emoid"  # 表情id
+    Flash = "Flash"  # 闪图
+    MsgId = "MsgId"  # 消息id
+    Reply = "Reply"  # 回复
+    Title = "Title"  # 用户头衔
+    Value = "Value"  # 值
+    Audio = "Audio"  # 音频
+    Video = "Video"  # 视频
+    Width = "Width"  # 宽度
+    Bubble = "Bubble"  # 气泡id
+    Height = "Height"  # 高度
+    Notice = "Notice"  # 公告
+    People = "People"  # 人数
+    Refresh = "Refresh"  # 刷新
+    UinName = "UinName"  # 名称 备注.....
+    UinNick = "UinNick"  # 昵称
+    Typeface = "Typeface"  # 字体
+    Withdraw = "Withdraw"  # 撤回
+    OpName = "OpName"  # 操作者名字
+    OpNick = "OpNick"  # 操作者昵称
+    Agree = "Agree"  # 同意
+    Refuse = "Refuse"  # 拒绝
+    Ignore = "Ignore"  # 忽略
+    Account = "Account"  # 登录账号
+    Open = "Open"  # 打开
+    Close = "Close"  # 关闭
+    MD5 = "MD5"  # MD5
+    Size = "Size"  # 大小
+    Offset = "Offset"  # 开始
+    Add = "Add"  # 增加
+    Remove = "Remove"  # 移除
+    Seq = "Seq"  # 序号
+    EmojiFace = "EmojiFace"  # 小黄脸表情
+    EmojiSuper = "EmojiSuper"  # 超级表情
+    EmojiSuperQQ = "EmojiSuperQQ"  # [超级QQ秀表情]OK
+    ProgressPush = "ProgressPush"  # 进度推送
+    PokeID = "PokeID"  # 戳一戳id
+    PokeIDSub = "PokeIDSub"  # 戳一戳子id
+    PokeMsg = "PokeMsg"  # 戳一戳消息
+    PokeSize = "PokeSize"  # 戳一戳大小
+    Dice = "Dice"  # 骰子
+    WindowJitter = "WindowJitter"  # 窗口抖动
+    FlashWord = "FlashWord"  # 闪字
+    FingerGuess = "FingerGuess"  # 猜拳
+    HeadPortrait = "HeadPortrait"  # 头像
+    AppId = "AppId"  # 账号
+
+    # 合并转发消息
+    MultiMsg = "MultiMsg"  # 消息合并转发
+    MultiMsgGet = "MultiMsgGet"  # 消息合并转发 获取
+    MultiMsgPut = "MultiMsgPut"  # 消息合并转发 保存
+
+    # 好友相关
+    Friend = "Friend"  # 好友消息
+    FriendListDisable = "FriendListDisable"  # 好友禁用列表
+    FriendListGet = "FriendListGet"  # 获取好友列表
+    FriendListGetName = "FriendListGetName"  # 获取好友备注
+    FriendListGetNick = "FriendListGetNick"  # 获取好友昵称
+    FriendBeatABeat = "FriendBeatABeat"  # 好友拍一拍
+    FriendMsgCacheGet = "FriendMsgCacheGet"  # 获取好友消息缓存
+
+    # 群聊相关
+    Group = "Group"  # 群聊消息
+    Owner = "Owner"  # 群主/频道主 QQ
+    GroupId = "GroupId"  # 群号
+    GroupName = "GroupName"  # 群名
+    GroupListDisable = "GroupListDisable"  # 群聊禁用列表
+    GroupListGet = "GroupListGet"  # 获取群聊列表
+    GroupListGetName = "GroupListGetName"  # 获取群聊名称
+    GroupMemberListGet = "GroupMemberListGet"  # 获取群聊成员列表
+    GroupMemberListGetAdmin = "GroupMemberListGetAdmin"  # 获取群聊管理员列表
+    GroupMemberListGetInactive = "GroupMemberListGetInactive"  # 获取群聊成员不活跃列表
+    GroupMemberListGetProhibit = "GroupMemberListGetProhibit"  # 获取群聊成员被禁言列表
+    GroupMemberListGetInfo = "GroupMemberListGetInfo"  # 获取群聊成员信息
+    GroupMemberSignout = "GroupMemberSignout"  # 群聊成员退出
+    GroupMemberNickModify = "GroupMemberNickModify"  # 修改群聊成员昵称
+    GroupModifyAdmin = "GroupModifyAdmin"  # 群聊管理员修改
+    GroupModifySpecialTitle = "GroupModifySpecialTitle"  # 修改群聊专属头衔
+    GroupNotify = "GroupNotify"  # 群通知
+    GroupProhibitAll = "GroupProhibitAll"  # 群聊全体禁言
+    GroupProhibitMember = "GroupProhibitMember"  # 群聊成员禁言
+    GroupMsgCacheGet = "GroupMsgCacheGet"  # 获取群聊消息缓存
+    GroupMsgAnonymous = "GroupMsgAnonymous"  # 群聊消息匿名
+    GroupMsgSetNotDisturb = "GroupMsgSetNotDisturb"  # 设置群聊消息免打扰
+    GroupAnonymous = "GroupAnonymous"  # 群聊匿名开关变动
+    GroupEnterState = "GroupEnterState"  # 进群状态
+    GroupInvitationFriend = "GroupInvitationFriend"  # 邀请好友进群
+    GroupNewMember = "GroupNewMember"  # 群聊新成员进群通知
+    GroupMusic = "GroupMusic"  # 一起听歌开关变动
+    GroupBeatABeat = "GroupBeatABeat"  # 群聊拍一拍
+    GroupEssence = "GroupEssence"  # 群聊精华
+    GroupDissolut = "GroupDissolut"  # 群聊解散
+    GroupClockin = "GroupClockin"  # 群聊打卡
+
+    # 群文件相关
+    GroupFile = "GroupFile"  # 群文件
+    GroupFileListGet = "GroupFileListGet"  # 获取列表
+    GroupFileUpload = "GroupFileUpload"  # 上传文件
+    GroupFileCreate = "GroupFileCreate"  # 创建文件夹
+    GroupFileRemove = "GroupFileRemove"  # 删除文件
+    GroupFileRemoveFolder = "GroupFileRemoveFolder"  # 删除文件夹
+    GroupFileMove = "GroupFileMove"  # 移动文件
+    GroupFileRename = "GroupFileRename"  # 重命名文件夹
+
+    GroupStick = "GroupStick"  # 群帖子
+
+    # 频道相关
+    Guild = "Guild"  # 频道消息
+    TinyId = "TinyId"  # 频道用户id
+    GuildId = "GuildId"  # 频道id
+    GuildCode = "GuildCode"  # 频道id号
+    ChannelId = "ChannelId"  # 频道id子
+    GuildType = "GuildType"  # 频道type
+    GuildName = "GuildName"  # 频道名
+    ChannelName = "ChannelName"  # 频道名子
+    GuildMsgCacheGet = "GuildMsgCacheGet"  # 获取频道消息缓存
+    GuildMemberSignout = "GuildMemberSignout"  # 频道成员退出
+    GuildEssence = "GuildEssence"  # 频道精华
+
+    # 系统相关
+    System = "System"  # 系统消息
+    Online = "Online"  # 查询在线状态
+    Goline = "Goline"  # 账号上线
+    GolineRetry = "GolineRetry"  # 重新上线
+    Offline = "Offline"  # 账号下线
+    GolineWindows = "GolineWindows"  # Windows端账号上线
+    OfflineWindows = "OfflineWindows"  # Windows端账号下线
+    Heartbeat = "Heartbeat"  # 心跳消息
+    HeartbeatLong = "HeartbeatLong"  # 长连心跳消息
+    FavoriteCard = "FavoriteCard"  # 名片赞
+    FavoriteCardListGet = "FavoriteCardListGet"  # 名片赞列表获取
+    FavoritePersonSign = "FavoritePersonSign"  # 个性签名赞
+    NewFriendNotify = "NewFriendNotify"  # 新朋友通知
+    UserSearch = "UserSearch"  # 搜索
+    UserInfoGet = "UserInfoGet"  # 获取上线信息
+    UserInfoModify = "UserInfoModify"  # 修改用户信息
+    UserGroupJoin = "UserGroupJoin"  # 加群
+    UserGroupSignout = "UserGroupSignout"  # 退群
+    UserFriendAdd = "UserFriendAdd"  # 加好友
+    UserFriendRemove = "UserFriendRemove"  # 删除好友
+    UserSigninDay = "UserSigninDay"  # 日签卡打卡
+    UserSigninNight = "UserSigninNight"  # 晚安卡打卡
+    OntimeTask = "OntimeTask"  # 整点任务
+    GolineMode = "GolineMode"  # 上线模式
+
+    # 空间相关
+    Qzone = "Qzone"  # 空间消息
+    SaySay = "SaySay"  # 说说
+
+    # 用户信息相关
+    Skey = "Skey"  # Skey
+    Age = "Age"  # 年龄
+    Gender = "Gender"  # 性别
+    Nick = "Nick"  # 昵称
+    Name = "Name"  # 备注
+    MemberLevel = "MemberLevel"  # 成员等级
+    JoinTime = "JoinTime"  # 加入时间
+    LastSpeakTime = "LastSpeakTime"  # 最后发言时间
+    SpecialTitle = "SpecialTitle"  # 专属头衔
+    Level = "Level"  # 等级
+    Location = "Location"  # 地区
+
+    # 系统功能
+    Debug = "Debug"  # 调试模式
+    CacheNewFile = "CacheNewFile"  # 获取临时文件
+    Unauthorized = "Unauthorized"  # 未授权
+    OicqWebScanCodeLogin = "OicqWebScanCodeLogin"  # 网站扫码登录
+
+    # JSON消息类型
+    SignJson = "SignJson"  # 签名卡片
+    CustomJson = "CustomJson"  # 自定义JSON消息
+    JSON_KG = "JSON_KG"  # 酷狗音乐
+    JSON_WY = "JSON_WY"  # 网易云音乐
+    JSON_QQ = "JSON_QQ"  # QQ音乐
+    JSON_KW = "JSON_KW"  # 酷我音乐
+    JSON_JSHU = "JSON_JSHU"  # 简书
+    JSON_BAIDU = "JSON_BAIDU"  # 百度
+    JSON_YK = "JSON_YK"  # 优酷
+    JSON_IQY = "JSON_IQY"  # 爱奇艺
+    JSON_BD = "JSON_BD"  # 波点
+    JSON_BL = "JSON_BL"  # 哔哩哔哩
+    JSON_KS = "JSON_KS"  # 快手
+    JSON_MG = "JSON_MG"  # 咪咕
+    JSON_QQLLQ = "JSON_QQLLQ"  # QQ浏览器
+    JSON_QQKJ = "JSON_QQKJ"  # QQ空间
+    JSON_5SING = "JSON_5SING"  # 5SING
+
+    # 日志打印
+    PrintE = "PrintE"  # 错误日志
+    PrintI = "PrintI"  # 信息日志
+    PrintV = "PrintV"  # 详细日志
+    PrintW = "PrintW"  # 警告日志
+
+    # 上线模式
+    GM_PO = "PO"  # 官方人机
+    GM_PA = "PA"  # 安卓手机
+    GM_PP = "PP"  # 安卓平板
+    GM_PL = "PL"  # 企鹅密码
+    GM_SA = "SA"  # 安卓手表(扫码登陆)
+    GM_SL = "SL"  # 企鹅扫码(扫码登陆)
---
+++ plugin/SecPlugin.py
@@ -0,0 +1,486 @@
+from typing import Union
+import websockets
+import asyncio
+import threading
+import traceback
+from time import sleep
+from datetime import datetime
+from json import dumps, loads
+import queue
+import logging
+from logging.handlers import QueueHandler, QueueListener
+from .Msg import Msg
+from .Messenger import Messenger
+from typing import Any
+from multiprocessing import Process, Queue as MPQueue, current_process
+from concurrent.futures import ProcessPoolExecutor
+
+class PluginConfig:
+    """
+    插件配置数据结构。
+
+    Attributes:
+        WS_URL (str): Secluded配置的WebSocket协议地址。
+        PID (str): 插件和Secluded对接的唯一ID。
+        NAME (str): 插件名称。
+        TOKEN (str): Secluded配置的WebSocket密钥。
+        PING_INTERVAL (Optional[int]): 每隔一段时间(单位秒)对WebSocket对方发送一次Ping帧，如果对方没有在规定时间内返回Pong WebSocket自动重连，填None代表不进行Ping。
+        LOG_LEVEL (int): 日志的详细程度。0 简略模式，1 详细模式，2 原始模式。
+    """
+    WS_URL = "ws://127.0.0.1:24804"
+    PID = "com.sumaroder.secplugin"
+    NAME = "SumaPlugin"
+    TOKEN = "SecretToken"
+    PING_INTERVAL = None
+    LOG_LEVEL = 0
+
+class SecPlugin:
+    """
+    插件主类，请继承重写此类。
+    """
+
+    def __init__(self, config: PluginConfig = PluginConfig()):
+        """
+        初始化SecPlugin。
+
+        Args:
+            config (PluginConfig, optional): 插件配置，默认为PluginConfig()。
+        """
+        self.seq: int = 1
+        self.ws: Any = None
+        self.running: bool = True
+        self.config: PluginConfig = config
+        self.GolineModes: dict[str, list[str]] = {
+            "SA": ["Scan Android", "安卓手表"],
+            "PA": ["Password Android", "安卓手机"],
+            "PP": ["Password Pad", "安卓平板"],
+            "SL": ["Scan Linux", "企鹅扫码"],
+            "PL": ["Password Linux", "企鹅密码"],
+            "PO": ["Password Official", "官方人机"]
+        }
+        self._setup_logging()
+        self.process_pool = ProcessPoolExecutor(max_workers=4)
+        self.message_queue = MPQueue()
+        self.send_queue = MPQueue()
+        self._setup_process_workers()
+
+    def _setup_process_workers(self):
+        """
+        设置工作进程。
+        """
+        def worker_process(queue):
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            while True:
+                try:
+                    msg = queue.get()
+                    if msg is None:
+                        break
+                    loop.run_until_complete(self.onMsgHandler(msg))
+                    self.log(f"[{current_process().name}] 处理完成")
+                except Exception as e:
+                    self.log(f"[{current_process().name}] 处理消息出错: {str(e)}")
+                except KeyboardInterrupt:
+                    self.log(f"{current_process().name} 终止")
+                    break
+        self.worker_processes = []
+        for i in range(4):
+            p = Process(
+                target=worker_process,
+                args=(self.message_queue,),
+                name=f"MsgWorker-{i}",
+                daemon=True
+            )
+            p.start()
+            self.worker_processes.append(p)
+
+    def _setup_logging(self):
+        """
+        配置日志系统。
+        """
+        self.logger = logging.getLogger(self.config.PID)
+        self.logger.setLevel(logging.DEBUG)
+
+        for handler in self.logger.handlers[:]:
+            self.logger.removeHandler(handler)
+
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+        console_formatter = logging.Formatter(
+            '[%(asctime)s | %(levelname)s] %(message)s',
+            datefmt='%H:%M:%S'
+        )
+        console_handler.setFormatter(console_formatter)
+        self.logger.addHandler(console_handler)
+
+        file_handler = logging.FileHandler("app.log", encoding="utf-8")
+        file_handler.setLevel(logging.DEBUG)
+        if self.config.LOG_LEVEL == 0:
+            file_formatter = logging.Formatter(
+                '[%(asctime)s | %(levelname)s]%(message)s',
+                datefmt='%H:%M:%S'
+            )
+        else:
+            file_formatter = logging.Formatter(
+                '[%(asctime)s][%(levelname)s]%(message)s',
+                datefmt='%Y-%m-%d | %H:%M:%S.%f'
+            )
+        file_handler.setFormatter(file_formatter)
+        self.logger.addHandler(file_handler)
+
+    def log(self, msg: str, level: int = logging.INFO):
+        """
+        添加日志。
+
+        Args:
+            msg (str): 日志消息。
+            level (int, optional): 日志级别，默认为logging.INFO。
+        """
+        self.logger.log(level, msg)
+
+    async def connect(self):
+        """
+        启动WebSocket对象，连接到Secluded，并接收Secluded传回的部分消息。
+        """
+        while self.running:
+            try:
+                async with websockets.connect(
+                    self.config.WS_URL,
+                    ping_interval=self.config.PING_INTERVAL,
+                    ping_timeout=30
+                ) as self.ws:
+                    await self.onOpen()
+                    while self.running:
+                        try:
+                            message = await asyncio.wait_for(self.ws.recv(), timeout=None)
+                            msg = loads(message)
+                            self.message_queue.put(msg)
+                        except (websockets.ConnectionClosed, ConnectionRefusedError) as e:
+                            raise e
+                        except Exception as e:
+                            traceback.print_exc()
+                            self.log(f"消息处理错误: {str(e)}", level=3)
+            except (websockets.ConnectionClosed, ConnectionRefusedError) as e:
+                self.log(f"连接中断: {str(e)}，3秒后重连...", level=2)
+                await asyncio.sleep(3)
+            except Exception as e:
+                self.log(f"发生错误: {str(e)}，3秒后重连...", level=3)
+                await asyncio.sleep(3)
+
+    async def onOpen(self):
+        """
+        WebSocket与Secluded连接成功后进行同步，才可以继续收到其他消息。
+        同步成功后，会收到一个Response消息，其中data.status为true。
+        """
+        await self.sendWss(
+            cmd="SyncOicq",
+            data={
+                "pid": self.config.PID,
+                "name": self.config.NAME,
+                "token": self.config.TOKEN
+            }
+        )
+
+    def _enum_keys_to_str(self, obj):
+        """
+        将Msg枚举类中的键转换为字符串。
+
+        Args:
+            obj (Any): 需要转换的对象。
+        
+        Returns:
+            Any: 转换后的对象。
+        """
+        if isinstance(obj, dict):
+            return { (k.value if isinstance(k, Msg) else k): self._enum_keys_to_str(v) for k, v in obj.items() }
+        elif isinstance(obj, list):
+            return [self._enum_keys_to_str(i) for i in obj]
+        elif isinstance(obj, Msg):
+            return obj.value
+        else:
+            return obj
+
+    async def sendWss(self, cmd: str, data: Any, needRsp: bool = True):
+        """
+        发送WebSocket到Secluded。
+
+        Args:
+            cmd (str): 指令。
+            data (Any): 内容。
+            needRsp (bool, optional): 是否需要等待响应，默认为True。
+        """
+        seq = self.seq
+        self.seq += 1
+        json_msg = {
+            "seq": seq,
+            "cmd": cmd,
+            "rsp": needRsp
+        }
+        if data is not None:
+            if isinstance(data, Messenger):
+                json_msg["data"] = self._enum_keys_to_str(data.getList())
+            else:
+                json_msg["data"] = self._enum_keys_to_str(data)
+
+        self.send_queue.put(dumps(json_msg))
+
+    async def sendMsg(self, messenger: Messenger, text: str):
+        """
+        自适应发送文本消息。
+
+        Args:
+            messenger (Messenger): 原消息的Messenger对象。
+            text (str): 文本内容。
+        """
+        if not text:
+            return
+        messenger = Messenger.getSendMessenger(messenger)
+        if messenger.getListSize() == 0:
+            self.log(f"无法自适应该消息体 | {messenger.toString()}")
+            return
+        messenger.addMsg(Msg.Text, text)
+        await self.sendWss(
+            cmd="SendOicqMsg",
+            data=messenger
+        )
+
+    async def sendPic(self, messenger: Messenger, url: str):
+        """
+        自适应发送图片消息。
+
+        Args:
+            messenger (Messenger): 原消息的Messenger对象。
+            url (str): 发送图片的链接/地址。
+        """
+        if not url:
+            return
+        messenger = Messenger.getSendMessenger(messenger)
+        if messenger.getListSize() == 0:
+            self.log(f"无法自适应该消息体 | {messenger.toString()}")
+            return
+        messenger.addMsg(Msg.Img, url)
+        await self.sendWss(
+            cmd="SendOicqMsg",
+            data=messenger
+        )
+
+    async def send(self, messenger: Messenger, msgList: list[list[Union[str, str]]]):
+        """
+        自适应发送自定义列表消息。
+
+        Args:
+            messenger (Messenger): 原消息的Messenger对象。
+            msgList (list[list[Union[int, str]]]): 消息列表，格式 [[Msg.Type, Content], ...]。
+        """
+        if not msgList:
+            return
+        messenger = Messenger.getSendMessenger(messenger)
+        if messenger.getListSize() == 0:
+            self.log(f"无法自适应该消息体 | {messenger.toString()}")
+            return
+        for l in msgList:
+            if len(l) == 2:
+                messenger.addMsg(l[0], l[1])
+        await self.sendWss(
+            cmd="SendOicqMsg",
+            data=messenger
+        )
+
+    async def onMsgHandler(self, msg: dict[str, Any]):
+        """
+        消息总处理。
+
+        Args:
+            msg (dict[str, Any]): 原消息。
+        """
+        try:
+            if msg.get("cmd") == "Response" and "seq" in msg:
+                seq = msg.get("seq")
+                self.log(f"[recvWss] -> {dumps(msg, ensure_ascii=False)}")
+                return
+
+            if self.config.LOG_LEVEL == 2:
+                self.log(f"{msg}", level=1)
+                return
+
+            if msg.get("cmd") == "Heartbeat":
+                await self.onHeartbeat(msg)
+            elif msg.get("cmd") == "Response":
+                data = msg.get("data", {})
+                if data.get("status"):
+                    self.log("服务端认证成功")
+                else:
+                    self.log(f"认证失败: {data}", level=2)
+            elif "data" in msg:
+                messenger = Messenger(msg.get("data", {}))
+                if msg.get("cmd") == "PushOicqMsg":    
+                    if messenger.hasMsg(Msg.Group):
+                        await self.onGroupMsgHandler(messenger)
+                    elif messenger.hasMsg(Msg.Friend):
+                        await self.onFriendMsgHandler(messenger)
+                    elif messenger.hasMsg(Msg.Temp):
+                        await self.onTempMsgHandler(messenger)
+                    elif messenger.hasMsg(Msg.Guild):
+                        await self.onGuildMsgHandler(messenger)
+                    elif messenger.hasMsg(Msg.Goline) or messenger.hasMsg(Msg.Offline):
+                        await self.onLine(messenger)
+                    elif messenger.hasMsg(Msg.OntimeTask):
+                        await self.onTimeTask(messenger)
+                    elif messenger.hasMsg(Msg.Heartbeat):
+                        await self.onAccountHeartbeat(messenger)
+                
+            else:
+                self.log(f"{msg.get('cmd', '')} - {msg.get('data', '无数据')}")
+        except Exception as e:
+            traceback.print_exc()
+            self.log(f"消息处理异常: {str(e)} | 原始数据: {msg}", level=3)
+
+    async def onTimeTask(self, messenger):
+        """
+        定时任务 5分钟一次，由SecPlugin.onMsgHandler回调执行。
+
+        Args:
+            messenger (Messenger): 原消息的Messenger对象。
+        """
+        self.log(f"[系统消息 | 定时任务] {messenger.getString(Msg.OntimeTask)}")
+
+
+    async def onLine(self, messenger):
+        """
+        账号上下线总处理，由SecPlugin.onMsgHandler回调执行。
+
+        Args:
+            messenger (Messenger): 原消息的Messenger对象。
+        """
+        mode = self.GolineModes.get(messenger.getString(Msg.GolineMode), ["未知模式", "Unknown"])
+        timestamp = messenger.getString(Msg.Goline) if messenger.hasMsg(Msg.Goline) else messenger.getString(Msg.Offline)
+        text = "上线" if messenger.hasMsg(Msg.Goline) else "下线"
+        self.log(f"账号[{messenger.getString(Msg.Account)}]{text} | {text}模式: {mode[1]}({mode[0]}) | {text}时间：{timestamp}")
+
+    async def onHeartbeat(self, msg):
+        """
+        框架心跳总处理，由SecPlugin.onMsgHandler回调执行。
+
+        Args:
+            msg (dict): 原消息。
+        """
+        self.log(f"框架心跳 | {msg.get('cmd-ver', 'unknown')}")
+        await self.sendWss(
+            cmd="Heartbeat",
+            data={
+                "pid": self.config.PID,
+                "name": self.config.NAME,
+                "token": self.config.TOKEN
+            }
+        )
+
+    async def onAccountHeartbeat(self, messenger):
+        """
+        账号心跳总处理，由SecPlugin.onMsgHandler回调执行。
+
+        Args:
+            messenger (Messenger): 原消息的Messenger对象。
+        """
+        mode = self.GolineModes.get(messenger.getString(Msg.GolineMode), ["未知模式", "Unknown"])
+        self.log(f"账号[{messenger.getString(Msg.Account)}]心跳: {messenger.getString(Msg.Heartbeat)} | 上线模式: {mode[1]}({mode[0]})")
+
+    async def onGroupMsgHandler(self, messenger):
+        """
+        群聊消息总处理，由SecPlugin.onMsgHandler回调执行。
+
+        Args:
+            messenger (Messenger): 原消息的Messenger对象。
+        """
+        self.log(f"群[{messenger.getString(Msg.GroupId)} | {messenger.getString(Msg.Uin)}]消息: {messenger.getString(Msg.Text)}")
+
+    async def onFriendMsgHandler(self, messenger):
+        """
+        好友消息总处理，由SecPlugin.onMsgHandler回调执行。
+
+        Args:
+            messenger (Messenger): 原消息的Messenger对象。
+        """
+        self.log(f"好友[{messenger.getString(Msg.Uin)}]消息: {messenger.getString(Msg.Text)}")
+
+    async def onTempMsgHandler(self, messenger):
+        """
+        临时消息总处理，由SecPlugin.onMsgHandler回调执行。
+
+        Args:
+            messenger (Messenger): 原消息的Messenger对象。
+        """
+        self.log(f"临时[{messenger.getString(Msg.GroupId)} | {messenger.getString(Msg.Uin)}]消息: {messenger.getString(Msg.Text)}")
+
+    async def onGuildMsgHandler(self, messenger):
+        """
+        频道消息总处理，由SecPlugin.onMsgHandler回调执行。
+
+        Args:
+            messenger (Messenger): 原消息的Messenger对象。
+        """
+        self.log(f"频道[{messenger.getString(Msg.GuildId)} | {messenger.getString(Msg.Uin)}]消息: {messenger.getString(Msg.Text)}")
+
+    def onClose(self):
+        """
+        关闭所有工作进程以及WebSocket连接。
+        """
+        for _ in self.worker_processes:
+            self.message_queue.put(None)
+        for p in self.worker_processes:
+            p.join(timeout=1)
+            if p.is_alive():
+                p.terminate()
+        self.running = False
+        if self.ws is not None:
+            asyncio.run(self.ws.close())
+            self.ws = None
+
+def start_websocket(plugin):
+    """
+    主进程。
+
+    Args:
+        plugin (SecPlugin): 插件实例。
+    """
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(plugin.connect())
+
+def sendQueueHandler(plugin):
+    """
+    进程不同于线程，消息并发时任何子进程(工作进程)都无法直接联系 plugin.ws 进行发送包 所以这里通过队列实现子进程与主进程的互通。
+
+    Args:
+        plugin (SecPlugin): 插件实例。
+    """
+    while plugin.running:
+        try:
+            msg = plugin.send_queue.get(timeout=1)
+            plugin.log(f"[sendWss] -> {msg}")
+            asyncio.run(plugin.ws.send(msg))
+        except queue.Empty:
+            continue
+
+def run(plugin):
+    """
+    启动一个插件实例。
+
+    Args:
+        plugin (SecPlugin): 插件实例。
+    """
+    ws_thread = threading.Thread(target=start_websocket, args=(plugin,), daemon=True)
+    ws_thread.start()
+
+    send_thread = threading.Thread(target=sendQueueHandler, args=(plugin,), daemon=True)
+    send_thread.start()
+
+    try:
+        while plugin.running:
+            pass
+    except KeyboardInterrupt:
+        plugin.running = False
+        plugin.onClose()
+        print("\n正在关闭...等待线程退出")
+        ws_thread.join(timeout=1)
+        send_thread.join(timeout=1)
+        print("程序终止")
