5bb334203be1ed5eb5f448a20b3aa47847b3226c
sumaroder <mcbbsxiaoliuaabb@outlook.com>
2025-08-11 20:27:02 +0800

    20250811-20:25:06

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
+# 编写指南：https://spdocs.tbit.xin/
+
 from SecPlugin import *
 
 plugin = Plugin(ws_url="ws://127.0.0.1:24804", token="SecretToken")
 
-@plugin.onMsg("测试(.*)?")
-def test(messenger: 'Messenger', matches):
-    if matches:
-        plugin.sendMsg(messenger, f"获取到：{matches.group(1)}")
-    plugin.log("消息接收", tag="onTest")
+@plugin.onMsg("测试")
+def test(messenger: 'Messenger') -> None:
     plugin.sendMsg(messenger, "成功")
 
-@plugin.onMsg("异步测试(.*)?")
-async def test_async(messenger: 'Messenger', matches):
-    if matches:
-        plugin.sendMsg(messenger, f"获取到：{matches.group(1)}")
-    plugin.log("消息接收", tag="onTestAsync")
-    plugin.sendMsg(messenger, "成功")
+@plugin.onMsg("测试(.*)")
+def test(messenger: 'Messenger', matches) -> None:
+    plugin.sendMsg(str(matches.groups()))
 
 plugin.start()
